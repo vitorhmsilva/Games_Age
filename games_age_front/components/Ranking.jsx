132380fb6f346c2d3350bda804764123eedcc887
Vitinho <vvitinho466@gmail.com>
2023-06-03 18:22:43 -0300

    Adição de alguns components no FrontEnd

---
+++ games_age_front/components/Ranking.jsx
@@ -0,0 +1,11 @@
+import styles from '@styles/Ranking.module.css'
+
+const Ranking = () => {
+  return (
+    <div className={styles.ranking_body}>
+        Ranking
+        </div>
+  )
+}
+
+export default Ranking
