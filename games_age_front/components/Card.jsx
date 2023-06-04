9ca665f547369a866972af4bdb015231ab3e86f3
João Pedro Alves <jotalmeida007@hotmail.com>
2023-06-04 00:07:30 -0300

    chore: update modules

--- games_age_front/components/Card.jsx
+++ games_age_front/components/Card.jsx
@@ -5,12 +5,12 @@ import styles from '@styles/Card.module.css';
 const Card = (props) => {
   return (
     <div className={styles.card_body}>
-      <div clasName={styles.logoContainer}>{props.gameImage}</div>
-      <div clasName={styles.nameContainer}>{props.gameName}</div>
-      <div clasName={styles.buttonsContainer}>apagar, e tchan</div>
-      <div clasName={styles.plataformsContainer}>{props.name}</div>
-      <div clasName={styles.progressContainer}>{props.progress}</div>
-      <div clasName={styles.achivmentsContainer}>{props.achivments}</div>
+      <div className={styles.logoContainer}>{props.gameImage}</div>
+      <div className={styles.nameContainer}>{props.gameName}</div>
+      <div className={styles.buttonsContainer}>apagar, e tchan</div>
+      <div className={styles.plataformsContainer}>{props.name}</div>
+      <div className={styles.progressContainer}>{props.progress}</div>
+      <div className={styles.achivmentsContainer}>{props.achivments}</div>
     </div>
   );
 };
